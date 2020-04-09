44efd64e4e6ee61afb17ed27ce3e4df0f132f8c9
Chang <cnxiong03@gmail.com>
2020-04-09 18:05:17 -0400

    created components folder in src folder and created home about contact navbar files

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='container'>
+            <h4 className='center'>About</h4>
+            <p>This is what we are About</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <h4 className='center'>Home</h4>
+            <p>You are now at the Home page</p>
+        </div>
+    )
+}
+
+export default Home
