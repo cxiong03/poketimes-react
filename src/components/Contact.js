18394a53c126939d87c863b4e867909238b5b1a0
Chang <cnxiong03@gmail.com>
2020-04-10 17:35:17 -0400

    redirecting page in Navbar.js and using higher order component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000)
     return (
         <div className='container'>
             <h4 className='center'>Contact</h4>
-            <p>This is the my Contact page</p>
+            <p>This is the Contact page</p>
         </div>
     )
 }
