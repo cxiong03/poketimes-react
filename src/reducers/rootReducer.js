12b7940a8f8fe46feb5100aa742afc54aad068ae
Chang <cnxiong03@gmail.com>
2020-04-18 19:39:03 -0400

    deleted componentDidMount function and state in Home.js then imported connect function and invoked it

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'Spirited Away', body: "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts."},
+        {id: '2', title: 'Princess Mononoke', body: "On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime."},
+        {id: '3', title: "Howl's Moving Castle", body: "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle."}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
